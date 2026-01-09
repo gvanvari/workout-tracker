ff89ee5f94a8c162e76c6d64e22494eade26bed4
gvanvari <gvanvari@gmail.com>
2026-01-08 20:13:07 -0500

    remove legacy code

--- src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-export interface AuthData {
-  password: string;
-}
-
-export interface LoginResponse {
-  success: boolean;
-  token?: string;
-  message?: string;
-}
--- src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const API_BASE_URL = "http://localhost:4000";
-export const MAX_RPE = 10;
-export const MIN_WEIGHT_REPS = 1;
-export const MAX_SETS = 10; // Assuming a maximum of 10 sets for simplicity
-export const DEFAULT_EXERCISE_NAME = "Unknown Exercise";
